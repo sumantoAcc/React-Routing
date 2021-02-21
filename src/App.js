090cd65a0cdf9b5013621782d5512fb7b9f2b712
sumanto pal <sumanto.pal@accolitedigital.com>
2021-02-21 19:28:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useRef, useEffect } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import ListBooks from './ListBooks';
+import Book from './Book';
+import Search from './Search'
 function App() {
+  const [books,setBooks] = useState([]);
+  useEffect(() => {
+    const temp=
+    [
+      {
+        name: "Designing Data-Intensive Applications",
+        author: "Martin Kleppmann",
+        Description : "Book on System Design"
+      },
+      {
+        name: "The Competetive Programming HandBook",
+        author: "Anitti Laaksonen",
+        Description : "Book on Competetive Programming on C++"
+      },
+      {
+        name: "Cracking the Coding Interview",
+        author: "gayle laakmann mcdowell",
+        Description : "Preparing for coding interviews of big-tech companies"
+      }
+    ];
+    setBooks(temp);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Router>
+  <Search books={books}/>
+  <Route path= {'/:name/:Description/:author'} exact  component={Book} />
+  <ListBooks books={books} />
+  </Router>
+  </div>
   );
+
 }
 
 export default App;
